ffaf02e3452aae3ee00e69aafcf25cf4e4e3600a
irastra <13572127415@163.com>
2022-07-25 22:55:03 +0800

    modify

--- src/triangle_color.cpp
+++ src/triangle_color.cpp
@@ -3,33 +3,22 @@
 #include <myshader.h>
 #include <myconst.h>
 #include <iostream>
+#include <mywindow.h>
+#include <myutil.h>
 
 using MyRender::Shader;
-
-void framebuffer_size_callback(GLFWwindow * window, int width, int height);
-void processInput(GLFWwindow* window);
+using MyRender::processInput;
+using MyRender::terminate_window;
+using MyRender::init_window;
+using MyUtil::get_res_path;
 
 int main(){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    GLFWwindow * window = glfwCreateWindow(MyRender::SCR_WIDTH, MyRender::SCR_HEIGHT, "triangle_color",NULL, NULL);
-    if (window == NULL){
-        std::cout << "Failed to initialize GLFW Window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    GLFWwindow * window = init_window();
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    Shader shader("C:/Users/zhangxu01/Documents/git/OpenGL/shader/3.3.shader.vs", "C:/Users/zhangxu01/Documents/git/OpenGL/shader/3.3.shader.fs");
+    Shader shader(get_res_path("shader/3.3.shader.vs"), get_res_path("shader/3.3.shader.fs"));
     float vertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
@@ -63,14 +52,4 @@ int main(){
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
-}
-
-void processInput(GLFWwindow* window){
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-
-void framebuffer_size_callback(GLFWwindow * window, int width, int height){
-    glViewport(0, 0, width, height);
 }
