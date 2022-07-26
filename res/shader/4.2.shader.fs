6718ad9db883d98bd380b656d9f58b9d9ef0b8e7
zhangxu01 <zhangxu01@corp.netease.com>
2022-07-26 12:03:03 +0800

    repair sampler2d

--- res/shader/4.2.shader.fs
+++ res/shader/4.2.shader.fs
@@ -5,6 +5,5 @@ in vec2 texCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main(){
-    //FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
-    FragColor = texture(texture1, texCoord);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
